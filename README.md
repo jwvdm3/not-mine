6cb1963b3d499d1758c17d74408609f0ec205114
John van der Meer <john.w.vdm@gmail.com>
2026-04-20 15:10:13 -0400

    added rubyslippers
    Replaced code.js, updated codepen reference in README.  Now click three times on profile icon to restore original page.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ This whimsical web page was developed in response to this prompt in Section 1.2.
 
 * Try to make a simple home page and add some interactivity to it, adapting the examples provided in the course (change the HTML content by clicking on a button, change the CSS style of some parts of the document).
 
-It will be shared there via CodePen '[My(?) Home Page]('My(?) Home Page')'.
+It will be shared there via [CodePen](https://codepen.io/jwvdm/pen/RNGddMG).
 
--- code.js
+++ code.js
@@ -18,24 +18,29 @@ function init() {
   resize();  // place the clone over the original image
   body.appendChild(clone);
 
-  button.addEventListener('click', ()=>{
-  	button.disabled = true;
+  function resize() {  // keep the clone over the original
+    outer = body.getBoundingClientRect(),
+    inner = image.getBoundingClientRect();
+    clone.style.top = (inner.top-outer.top) + 'px'; 
+    clone.style.left = (inner.left-outer.left) + 'px';
+  }
+
+  let counter = 0, finalize;
+  button.addEventListener('click', ()=>{ 
+  	  changeTo(withoutMe); 
+  	  button.disabled = true;
+  	}, false);
+
+  function changeTo(newPage) {
   	clone.style.transition = 'all 500ms ease-in-out';
   	clone.style.width = '100vw';
   	clone.style.height = '100vh';
     clone.style.top = 0; 
     clone.style.left = 0;
 
-    setTimeout(()=>{  // return clone to frame 
-      // change the page
-      main.querySelector('header').style.opacity = 0;
-      button.style.display = 'none';
-      details.style.margin = '30px';
-      details.style.textAlign = 'center';
-      details.style.fontSize = '120%';
-      details.querySelector('summary').style.fontSize = '100%';
-      details.open = true;
-      
+    setTimeout(()=>{ 
+      newPage();  // change the page behind image
+
       // put the image back into its frame
       clone.style.transitionDuration = '1500ms';
       clone.style.width = inner.width + 'px';
@@ -45,14 +50,48 @@ function init() {
 
       setTimeout (()=>{  // final clean up
         clone.style.transition = 'none';
+        finalize();
       }, 1700);
     }, 1000);
-  }, false);
-
-  function resize() {  // keep the clone over the original
-    outer = body.getBoundingClientRect(),
-    inner = image.getBoundingClientRect();
-    clone.style.top = (inner.top-outer.top) + 'px'; 
-    clone.style.left = (inner.left-outer.left) + 'px';
+  }
+  function withoutMe() {
+    main.querySelector('header').style.opacity = 0;
+    button.style.display = 'none';
+    details.style.margin = '30px';
+    details.style.textAlign = 'center';
+    details.style.fontSize = '120%';
+    details.querySelector('summary').style.fontSize = '100%';
+    details.open = true;
+    finalize = ()=>{
+      clone.addEventListener('click', rubySlippers, false);
+      clone.title = 'Click three times to restore the original page.';
+    };
+  }
+  function rubySlippers() {  // click three times to get back to ...
+    switch(counter) {
+    case 0:
+      setTimeout(()=>{
+          counter = 0;
+          timeout = undefined;
+        }, 1000);  // 1 second time limit
+      // fall through
+    case 1:
+      counter++;
+      break;
+    default:  // more than two clicks
+      clone.removeEventListener('click', rubySlippers, false);
+      clone.title = '';
+      changeTo(withMe);
+    }
+  }
+  function withMe() {
+    main.querySelector('header').style.opacity = 1;
+    button.style.display = 'inline-block';
+    details.style.margin = '-3px 0';
+    details.style.textAlign = 'left';
+    details.style.fontSize = '100%';
+    details.querySelector('summary').style.fontSize = '75%';
+    details.open = false;
+    finalize = ()=>{ button.disabled = false; };
   }
 }
